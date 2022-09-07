100daa2371b5d956f370e450b11f875d89c02c60
desonglll <lindesong666@163.com>
2022-09-07 09:48:55 +0800

    first commit

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+echo "# obsidian" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:desonglll/obsidian.git
+git push -u origin main
